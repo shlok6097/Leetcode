4a3dc79d79a6cd38f1e4254bace9db2e88581def
shlok dakulgi <shlokdakulgi6097@gmail.com>
2026-02-23 22:54:49 +0530

    Time: 6 ms (39.26%) | Memory: 45.1 MB (68.19%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+          Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i=0,j=0,k=0;
+        int[] ans = new int[Math.min(nums1.length, nums2.length)];
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i] < nums2[j]){
+                i++;
+            }else if(nums1[i] > nums2[j]){
+                j++;
+            }else {
+       
+                if ( nums1[i] == nums2[j]) {
+                    ans[k++] = nums1[i];
+                }
+
+                 i++;
+                 j++;
+            }
+            
+           
+
+        }
+        return Arrays.copyOf(ans, k);
+    }
+}
