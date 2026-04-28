088f770e0bfb5f799120cc430a8902309df29a8e
shlok dakulgi <shlokdakulgi6097@gmail.com>
2026-04-28 19:42:39 +0530

    Time: 2 ms (5.11%) | Memory: 44.5 MB (7.65%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+      Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        find(0,nums,new ArrayList<>(),ans);
+        return ans;
+        
+    }
+    public void find(int k, int[]num, List<Integer> ds, List<List<Integer>> ans){
+        ans.add(new ArrayList<>(ds));
+
+        for(int i =k;i<num.length;i++){
+            if(i !=k && num[i]==num[i-1]) continue;
+            //check the uniqueness 
+            ds.add(num[i]);
+            find(i+1,num,ds,ans);
+            ds.remove(ds.size()-1);
+
+
+        }
+    }
+}
