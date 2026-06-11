477b18dd316621aef51aa0f25257c8d3644bf858
shlok dakulgi <shlokdakulgi6097@gmail.com>
2026-06-11 21:02:17 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (99.07%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,27 @@
+class Solution {
+    public double myPow(double x, int n) {
+        
+        long N = n;
+        
+        
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        double res = 1.0;
+        double currentProduct = x;
+        
+        while (N > 0) {
+            
+            if (N % 2 == 1) {
+                res = res * currentProduct;
+            }
+           
+            currentProduct = currentProduct * currentProduct;
+            N /= 2;
+        }
+        
+        return res;
+    }
+}
