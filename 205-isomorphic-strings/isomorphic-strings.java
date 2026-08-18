cbbd5176c25d147a70e54d6a3020a4129a86aec3
shlok dakulgi <shlokdakulgi6097@gmail.com>
2026-08-18 19:22:38 +0530

    Time: 11 ms (67.83%) | Memory: 43.6 MB (87.06%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+      
+        Map<Character, Character> map = new HashMap<>();
+
+        for(int i =0; i<s.length();i++){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            if(map.containsKey(ch1)){
+                if(map.get(ch1) != ch2) return false;
+            }else{
+                if(map.containsValue(ch2)) return false;
+                map.put(ch1,ch2);
+            }
+        }
+        return true;
+
+        
+    }
+    private int[] freq(String s){
+        int[] fre = new int[100];
+
+        for(char ch: s.toCharArray()){
+            fre[ch - '0']++;
+        }
+        return fre;
+    }
+}
